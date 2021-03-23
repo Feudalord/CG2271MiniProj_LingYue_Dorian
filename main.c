6c6eaeb5371ab9a9ccea9d6d594c6bd1096c48f8
Feudalord <Feudalord@live.co.uk>
2021-03-23 10:46:54 +0800

    Update

--- main.c
+++ main.c
@@ -6,23 +6,70 @@
 #include  CMSIS_device_header
 #include "cmsis_os2.h"
  
+#define RED_LED 18
+#define GREEN_LED 19
+#define BLUE_LED 1
+#define MASK(x) (1 << (x))
+ 
 /*----------------------------------------------------------------------------
  * Application main thread
  *---------------------------------------------------------------------------*/
-void app_main (void *argument) {
- 
-  // ...
-  for (;;) {}
+void tBrain (void *argument) {
+  for (;;) {
+	}
+}
+
+void tMotorControl (void *argument) {
+	for (;;) {
+	}
+}
+
+void tLED (void *argument) {
+  for (;;) {
+	}
+}
+
+void tAudio (void *argument) {
+	for (;;) {
+	}
+}
+
+void InitGPIO(void)
+{
+	// Enable Clock to PORTB and PORTD
+	SIM->SCGC5 |= ((SIM_SCGC5_PORTB_MASK) | (SIM_SCGC5_PORTD_MASK));
+	// Configure MUX settings to make all 3 pins GPIO
+	PORTB->PCR[RED_LED] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[RED_LED] |= PORT_PCR_MUX(1);
+	PORTB->PCR[GREEN_LED] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[GREEN_LED] |= PORT_PCR_MUX(1);
+	PORTD->PCR[BLUE_LED] &= ~PORT_PCR_MUX_MASK;
+	PORTD->PCR[BLUE_LED] |= PORT_PCR_MUX(1);
+	
+	PTB->PDDR |= (MASK(RED_LED) | MASK(GREEN_LED));
+	PTD->PDDR |= MASK(BLUE_LED);
+	
+}
+
+void offRGB(void){
+		PTB->PSOR = MASK(RED_LED);
+		PTB->PSOR = MASK(GREEN_LED);
+		PTD->PSOR = MASK(BLUE_LED);
 }
  
 int main (void) {
  
   // System Initialization
   SystemCoreClockUpdate();
+	InitGPIO();
+	offRGB();
   // ...
  
   osKernelInitialize();                 // Initialize CMSIS-RTOS
-  osThreadNew(app_main, NULL, NULL);    // Create application main thread
+  osThreadNew(tBrain, NULL, NULL);    // Create application main thread
+	osThreadNew(tMotorControl, NULL, NULL);
+	osThreadNew(tLED, NULL, NULL);
+	osThreadNew(tAudio, NULL, NULL);
   osKernelStart();                      // Start thread execution
   for (;;) {}
 }
